78cb9cfa49d0f11cb856a7321c447892c69da46d
Gavin Lon <42768005+GavinLonDigital@users.noreply.github.com>
2022-01-27 10:25:10 +0200

    Added helpful comment "Important Instruction"

--- ShopOnline.Api/Program.cs
+++ ShopOnline.Api/Program.cs
@@ -12,6 +12,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//***IMPORTANT INSTRUCTION HERE - MUST CONFIGURE CONNECTION BEFORE RUNNING MIGRATIONS
 builder.Services.AddDbContextPool<ShopOnlineDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("ShopOnlineConnection"))
 );
